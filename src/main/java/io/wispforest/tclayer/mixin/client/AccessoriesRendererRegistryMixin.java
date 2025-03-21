0fb0ba89b7e3403aa5e18b0734fb2e7a1c257c90
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-20 21:55:44 -0500

    Update mixin injection to prevent trinket items from getting default renderer

--- src/main/java/io/wispforest/tclayer/mixin/client/AccessoriesRendererRegistryMixin.java
+++ src/main/java/io/wispforest/tclayer/mixin/client/AccessoriesRendererRegistryMixin.java
@@ -1,27 +1,25 @@
 package io.wispforest.tclayer.mixin.client;
 
+import com.google.common.collect.BiMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.emi.trinkets.api.TrinketsApi;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
-import io.wispforest.accessories.api.client.AccessoryRenderer;
 import io.wispforest.tclayer.compat.WrappedAccessory;
 import net.minecraft.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Map;
-
 @Mixin(value = AccessoriesRendererRegistry.class)
 public abstract class AccessoriesRendererRegistryMixin {
-    @WrapOperation(method = "getRender(Lnet/minecraft/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Ljava/util/Map;getOrDefault(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"))
-    private static Object alterDefaultBehavior(Map<Item, AccessoryRenderer> CACHED_RENDERERS, Object item, Object defaultRenderer, Operation<Object> operation) {
+    @WrapOperation(method = "getRenderer(Lnet/minecraft/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/BiMap;containsKey(Ljava/lang/Object;)Z"))
+    private static boolean alterDefaultBehavior(BiMap map, Object item, Operation<Boolean> operation) {
         var trinket = TrinketsApi.getTrinket((Item) item);
 
         if(!(trinket == TrinketsApi.getDefaultTrinket() || trinket instanceof WrappedAccessory)) {
-            defaultRenderer = null;
+            return true;
         }
 
-        return operation.call(CACHED_RENDERERS, item, defaultRenderer);
+        return operation.call(map, item);
     }
 }

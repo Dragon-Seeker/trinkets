28adff6e8f6691ab10297cc1b4d643414a636df4
Dragon-Seeker <timekeeperguild@gmail.com>
2025-03-21 00:17:56 -0500

    Fix incorrect arg for getting possible trinket

--- src/main/java/io/wispforest/tclayer/mixin/client/AccessoriesRendererRegistryMixin.java
+++ src/main/java/io/wispforest/tclayer/mixin/client/AccessoriesRendererRegistryMixin.java
@@ -3,6 +3,7 @@ package io.wispforest.tclayer.mixin.client;
 import com.google.common.collect.BiMap;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.emi.trinkets.api.TrinketsApi;
 import io.wispforest.accessories.api.client.AccessoriesRendererRegistry;
 import io.wispforest.tclayer.compat.WrappedAccessory;
@@ -13,13 +14,13 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(value = AccessoriesRendererRegistry.class)
 public abstract class AccessoriesRendererRegistryMixin {
     @WrapOperation(method = "getRenderer(Lnet/minecraft/item/Item;)Lio/wispforest/accessories/api/client/AccessoryRenderer;", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/BiMap;containsKey(Ljava/lang/Object;)Z"))
-    private static boolean alterDefaultBehavior(BiMap map, Object item, Operation<Boolean> operation) {
-        var trinket = TrinketsApi.getTrinket((Item) item);
+    private static boolean alterDefaultBehavior(BiMap map, Object key, Operation<Boolean> operation, @Local(argsOnly = true) Item item) {
+        var trinket = TrinketsApi.getTrinket(item);
 
         if(!(trinket == TrinketsApi.getDefaultTrinket() || trinket instanceof WrappedAccessory)) {
             return true;
         }
 
-        return operation.call(map, item);
+        return operation.call(map, key);
     }
 }
